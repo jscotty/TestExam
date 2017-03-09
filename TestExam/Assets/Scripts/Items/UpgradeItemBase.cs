aae94e2acabe1ba659e138625602a396163d1ced
bradakan <rocky-tempelaars@live.nl>
2017-03-09 11:36:17 +0100

    made a start to the items

---
+++ TestExam/Assets/Scripts/Items/UpgradeItemBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeItemBase : MonoBehaviour {
+
+	[SerializeField] protected float timeForIngotToBeReady;
+    [SerializeField] protected GameObject handoutReadyIcon;
+    [SerializeField] protected GameObject objecToHandOut;
+
+    public virtual void StartTimeTillhandout()
+    {
+        StartCoroutine("TimeTillhandout");
+    }
+
+    protected virtual IEnumerator TimeTillhandout()
+    {
+        yield return new WaitForSeconds(timeForIngotToBeReady);
+        HandoutReady();
+    }
+
+    protected virtual void HandoutReady()
+    {
+        handoutReadyIcon.SetActive(true);
+    }
+
+    protected virtual GameObject CollectHandout()
+    {
+        handoutReadyIcon.SetActive(false);
+        return Instantiate(objecToHandOut);
+    }
+}
