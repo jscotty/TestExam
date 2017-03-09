6d496b52a6a82e81f3e0fdac03711031606515e7
Nick van Dokkum <nickvandokkum@hotmail.nl>
2017-03-09 11:26:52 +0100

    Made test scene and set up base for in game UI

---
+++ TestExam/Assets/Scripts/UI/InGame/ItemImageSelect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemImageSelect : MonoBehaviour {
+
+    [SerializeField] private List<Sprite> _completeItemSprites = new List<Sprite>();
+    [SerializeField] private List<Sprite> _materialSprites = new List<Sprite>();
+    [SerializeField] private Image _completeItemImage;
+    [SerializeField] private Image _material0;
+    [SerializeField] private Image _material1;
+
+	public void SetImages(int iRecipe) {
+        _completeItemImage.sprite = _completeItemSprites[iRecipe];
+        if(iRecipe == 0) {
+            _material0.sprite = _materialSprites[0];
+            _material1.sprite = _materialSprites[0];
+        }
+        else if (iRecipe == 1) {
+            _material0.sprite = _materialSprites[0];
+            _material1.sprite = _materialSprites[1];
+        }
+        else if (iRecipe == 2) {
+            _material0.sprite = _materialSprites[1];
+            _material1.sprite = _materialSprites[1];
+        }
+    }
+}
---
+++ TestExam/Assets/Scripts/UI/InGame/ItemsToBuild.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsToBuild : MonoBehaviour {
+
+    [SerializeField] private GameObject _itemUIPrefab;
+    private List<GameObject> _currentRecipes = new List<GameObject>();
+
+    private void Start() {
+        CreateRecipe();
+    }
+
+    public void CreateRecipe (int iRecipe = 0) {
+        if(iRecipe == 0) {
+            iRecipe = Random.Range(1, 4);
+            Debug.Log(iRecipe);
+        }
+        GameObject tItemUI = Instantiate(_itemUIPrefab, this.transform, false);
+        tItemUI.transform.position = transform.position;
+        tItemUI.GetComponent<ItemImageSelect>().SetImages(iRecipe - 1);
+        _currentRecipes.Add(tItemUI);
+    }
+}
