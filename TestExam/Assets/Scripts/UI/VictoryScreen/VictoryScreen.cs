2d7ed7188190ee3fbf0813c8d9eff07fa63e882d
Nick van Dokkum <nickvandokkum@hotmail.nl>
2017-03-20 12:31:34 +0100

    working on the victory screen

--- TestExam/Assets/Scripts/UI/VictoryScreen/VictoryScreen.cs
+++ TestExam/Assets/Scripts/UI/VictoryScreen/VictoryScreen.cs
@@ -6,26 +6,44 @@ using UnityEngine.UI;
 public class VictoryScreen : MonoBehaviour {
 
     [SerializeField]
-    private List<Text> _scoreText = new List<Text>();
+    private List<Text> _scoreText = new List<Text>(4);
     [SerializeField]
-    private List<Text> _totalScores = new List<Text>();
+    private List<Text> _totalScores = new List<Text>(4);
     [SerializeField]
-    private List<GameObject> _playerPortraits = new List<GameObject>();
+    private List<GameObject> _playerPortraits = new List<GameObject>(4);
     [SerializeField]
     private List<Image> _scorePlates = new List<Image>(4);
     [SerializeField]
     private Sprite _mvpPlate;
-    
 
-	public void SetVictoryScreen() {
+    private void Start() {
+        GameInfoTracker.Instance.AddScore(6, 4, 3);
+        GameInfoTracker.Instance.AddScore(3, 7, 1);
+        GameInfoTracker.Instance.AddScore(8, 2, 2);
+    }
+
+    public void SetVictoryScreen() {
         int tSaboteur = Random.Range(0, 5);
         _playerPortraits[tSaboteur].transform.Translate(Vector2.up * 50);
-        List<int> tRecipeScores = GameInfoTracker.Instance._playerScores;
-        List<int> tSaboteurScores = GameInfoTracker.Instance._SaboteurScores;
+        List<int> tRecipeScores = GameInfoTracker.Instance.PlayerScores;
+        List<int> tSaboteurScores = GameInfoTracker.Instance.SaboteurScores;
+        List<int> tTotalScores = new List<int>();
+
+        int tHighestScore = 0;
 
         for (int i = 0; i < 4; i++) {
+            tTotalScores.Add(tRecipeScores[i] + tSaboteurScores[i]);
             _scoreText[i].text = tRecipeScores[i] + "\n" + tSaboteurScores[i];
-            _totalScores[i].text = (tRecipeScores[i] + tSaboteurScores[i]).ToString();
+            _totalScores[i].text = tTotalScores[i].ToString();
+            if (tTotalScores[i] > tHighestScore) {
+                tHighestScore = tTotalScores[i];
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            if(tTotalScores[i] == tHighestScore) {
+                _scorePlates[i].sprite = _mvpPlate;
+                _playerPortraits[i].transform.Translate(Vector2.up * 50);
+            }
         }
     }
 }
