1f0e8c5d2df15bf3d708fe2e67a2beebd5c62ab8
jscotty <jscotty@hotmail.nl>
2017-03-24 10:24:01 +0100

    bake.

--- TestExam/Assets/Scripts/UI/uGUITools.cs
+++ TestExam/Assets/Scripts/UI/uGUITools.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 public class uGUITools : MonoBehaviour {
@@ -94,3 +95,4 @@ public class uGUITools : MonoBehaviour {
 		}
 	}
 }
+#endif
